fe5626515958cd50bcb6f8a001132addfca2cdd1
Jose Antonio Aguilar <aaguilar.proyectos@gmail.com>
2018-02-01 13:26:31 -0600

    Project created ...

---
+++ src/main/java/com/blackbox/sharing/SharingApplication.java
@@ -0,0 +1,12 @@
+package com.blackbox.sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharingApplication.class, args);
+	}
+}
