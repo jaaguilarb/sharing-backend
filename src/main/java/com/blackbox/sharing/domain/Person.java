b2d55f421a0caff1b5216be82c5a7258e0b6d0d9
Jose Antonio Aguilar <aaguilar.proyectos@gmail.com>
2018-02-01 21:01:36 -0600

    Add domain class - Person

---
+++ src/main/java/com/blackbox/sharing/domain/Person.java
@@ -0,0 +1,113 @@
+/**
+ * 
+ */
+package com.blackbox.sharing.domain;
+
+import java.io.Serializable;
+
+/**
+ * @author joseph
+ *
+ */
+public class Person implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String name;
+	private String email;
+
+	/**
+	 * 
+	 */
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param id
+	 * @param name
+	 * @param email
+	 */
+	public Person(Integer id, String name, String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
--- src/main/java/com/blackbox/sharing/ws/PersonRest.java
+++ src/main/java/com/blackbox/sharing/ws/PersonRest.java
@@ -3,9 +3,14 @@
  */
 package com.blackbox.sharing.ws;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.blackbox.sharing.domain.Person;
 
 /**
  * @author joseph
@@ -15,7 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/persons")
 public class PersonRest {
 	@RequestMapping(method=RequestMethod.GET)
-	public String test() {
-		return "Test working...";
+	public List<Person> test() {
+		Person p1 = new Person(1, "Alba", "alba@gmail.com");
+		Person p2 = new Person(2, "Ikram", "ikram@email.com");
+		Person p3 = new Person(3, "Alba", "danis@email.com");
+		List<Person> list = new ArrayList<>();
+		list.add(p1);
+		list.add(p2);
+		list.add(p3);
+
+		return list;
 	}
 }
