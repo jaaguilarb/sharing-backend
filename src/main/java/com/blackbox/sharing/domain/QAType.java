5c0d73b32736b62de30d0227f74bf76a3e1eaf69
Jose Antonio Aguilar <aaguilar.proyectos@gmail.com>
2018-02-21 14:00:37 -0600

    The enums were modified QAType and ResourceType.

--- src/main/java/com/blackbox/sharing/domain/QAType.java
+++ src/main/java/com/blackbox/sharing/domain/QAType.java
@@ -1,12 +1,52 @@
 package com.blackbox.sharing.domain;
 
 public enum QAType {
-	ONLY_ONE_OPTION,
-    MULTIPLE_CHOICE,
-    YES_NO,
-    TRUE_FALSE,
-    FILL_IN_THE_BLANK,
-    MATCHING,
-    ORDERING,
-    COMPUTATIONAL;
+	ONLY_ONE_OPTION(1, "Only one option"),
+    MULTIPLE_CHOICE(2, "Multiple choice"),
+    YES_NO(3, "Yes/No"),
+    TRUE_FALSE(4, "True/False"),
+    FILL_IN_THE_BLANK(5, "Fill in the blank"),
+    MATCHING(6, "Matching"),
+    ORDERING(7, "Ordering"),
+    COMPUTATIONAL(8, "Computational");
+
+	private Integer code;
+	private String description;
+
+	/**
+	 * @param code
+	 * @param description
+	 */
+	private QAType(Integer code, String description) {
+		this.code = code;
+		this.description = description;
+	}
+
+	/**
+	 * @return the code
+	 */
+	public Integer getCode() {
+		return code;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * Get enum type from code
+	 */
+	public static QAType toEnum(Integer code) {
+		if (code == null)
+			return null;
+		for (QAType qa : QAType.values()) {
+			if (code.equals(qa.getCode())) {
+				return qa;
+			}
+		}
+		throw new IllegalArgumentException("Invalid Id: " + code);
+	}
 }
--- src/main/java/com/blackbox/sharing/domain/ResourceType.java
+++ src/main/java/com/blackbox/sharing/domain/ResourceType.java
@@ -1,7 +1,47 @@
 package com.blackbox.sharing.domain;
 
 public enum ResourceType {
-    VIDEO,
-    DOCUMENT,
-    FILE;
+    VIDEO(1, "Video"),
+    DOCUMENT(2, "Document"),
+    FILE(3, "File");
+
+	private Integer code;
+	private String description;
+
+	/**
+	 * @param code
+	 * @param description
+	 */
+	private ResourceType(Integer code, String description) {
+		this.code = code;
+		this.description = description;
+	}
+
+	/**
+	 * @return the code
+	 */
+	public Integer getCode() {
+		return code;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * Get enum type from code
+	 */
+	public static ResourceType toEnum(Integer code) {
+		if (code == null)
+			return null;
+		for (ResourceType qa : ResourceType.values()) {
+			if (code.equals(qa.getCode())) {
+				return qa;
+			}
+		}
+		throw new IllegalArgumentException("Invalid Id: " + code);
+	}
 }
